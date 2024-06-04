93ee5155336f13b400ad73bb5f8c2ca23c1a7f7a
Daniel Caballero <danielcaballero88@gmail.com>
2024-06-04 08:57:19 +0200

    Add mock users data

---
+++ accounts/mocks/mock_data.py
@@ -0,0 +1,21 @@
+# Mock users
+users = {
+    "admin": {
+        "username": "admin",
+        "email": "admin@bgs.com",
+        "password": "admin",
+    },
+    "player1": {
+        "username": "player1",
+        "email": "player1@bgs.com",
+        "password": "player1",
+    },
+    "player2": {
+        "username": "player2",
+        "email": "player2@bgs.com",
+        "password": "player2",
+    },
+}
+
+# Which users are superusers
+superusers = ["admin"]
