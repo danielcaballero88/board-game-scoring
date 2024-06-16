18266a010138e3337d8ad0c973757fb6c15e4236
Daniel Caballero <danielcaballero88@gmail.com>
2024-06-16 09:01:04 +0200

    Create form to add a new score to a table

--- scoring/views/score_create.py
+++ scoring/views/score_create.py
@@ -2,8 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from ..models import Table
+
 
 @login_required(login_url="/accounts/login")
 def score_create(request: HttpRequest, table_pk: int):
-    context = {"table_pk": table_pk}
+    table = Table.objects.get(pk=table_pk)
+    context = {"table": table}
     return render(request, "scoring/score_create.html", context)
