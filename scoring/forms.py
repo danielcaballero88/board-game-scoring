65944afbbc714b38def914449966b6b2b4aaa10f
Daniel Caballero <danielcaballero88@gmail.com>
2024-06-19 11:52:46 +0200

    Add validation to score create form

--- scoring/forms.py
+++ scoring/forms.py
@@ -9,6 +9,17 @@ def get_scoring_form(table: Table):
 
         name = forms.CharField(max_length=50)
 
+        # Add validation for `name` field to prevent duplicate names
+        # in the same table.
+        def clean_name(self):
+            name = self.cleaned_data["name"]
+            if (
+                table.ot_players.filter(name=name).exists()
+                or table.players.filter(user__username=name).exists()
+            ):
+                raise forms.ValidationError("Name already exists in this table.")
+            return name
+
     for sc in table.game.scoring_categories.all():
         field = forms.IntegerField(label=sc.name)
         ScoringForm.base_fields[sc.safe_string] = field
